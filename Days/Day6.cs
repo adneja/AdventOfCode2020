829bc8ede00e327469ee64c0c18e41373133ea0a
Ådne Jacobsen <adnejacobsen@Adnes-MacBook-Pro.local>
2020-12-06 20:32:18 +0100

    day 6 part one

--- Days/Day6.cs
+++ Days/Day6.cs
@@ -1,17 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Aoc.Classes;
 
 namespace Aoc.Days
 {
     public class Day6 : Day, IDay
     {
+        private readonly Dictionary<int,string> declarations;
+        
         public Day6(string path) : base(path)
         {
-            
+            declarations = new Dictionary<int,string>();
+            int groupId = 0;
+
+            foreach(string line in puzzleInput)
+            {
+                if(line.Length == 0) groupId++;
+                
+                if(!declarations.ContainsKey(groupId))
+                {
+                    declarations.Add(groupId, line);
+                }
+                else
+                {
+                    foreach(char c in line)
+                    {
+                        if(!declarations[groupId].Contains(c)) declarations[groupId] += c;
+                    }
+                }
+            }
         }
 
         public int PartOne()
         {
-            return -1;
+            return declarations.Values.Aggregate((acc, curr) => acc += curr).Length;
         }
 
         public int PartTwo()
